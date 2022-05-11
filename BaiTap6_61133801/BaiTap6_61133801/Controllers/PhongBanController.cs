f15642712cbc0de33e7b885ba287c2e55058ee3d
Khanh Tran <khanh.tv.soen@gmail.com>
2022-05-12 02:14:58 +0700

    bai tap 6

---
+++ BaiTap6_61133801/BaiTap6_61133801/Controllers/PhongBanController.cs
@@ -0,0 +1,117 @@
+﻿using BaiTap6_61133801.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTap6_61133801.Controllers
+{
+    public class PhongBanController : Controller
+    {
+        QLNV_MVC_61133801Entities db = new QLNV_MVC_61133801Entities();
+        // GET: PhongBan
+        public ActionResult Index()
+        {
+            List<PHONGBAN> pb = db.PHONGBAN.ToList();
+            return View(pb);
+        }
+
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PHONGBAN phongBan = db.PHONGBAN.Find(id);
+            if (phongBan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(phongBan);
+        }
+  
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaPB,TenPB")] PHONGBAN phongBan)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PHONGBAN.Add(phongBan);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(phongBan);
+        }
+
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+           PHONGBAN phongBan = db.PHONGBAN.Find(id);
+            if (phongBan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(phongBan);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaPB,TenPB")] PHONGBAN phongBan)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(phongBan).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(phongBan);
+        }
+
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PHONGBAN phongBan = db.PHONGBAN.Find(id);
+            if (phongBan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(phongBan);
+        }
+
+        // POST: PhongBans/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            PHONGBAN phongBan = db.PHONGBAN.Find(id);
+            db.PHONGBAN.Remove(phongBan);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+    }
+}
